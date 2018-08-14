f62043ad31031c0ceb500ce460dc95b46a60b331
Paulo Filho <pcf.feat@gmail.com>
2018-08-13 20:04:55 -0400

    Cart with added items

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    total: 0
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' }
+    ],
+    cart: []
   },
   methods: {
-    addItem: function () {
+    addItem: function (index) {
       this.total += 9.99;
+      this.cart.push(this.items[index]);
     }
   }
 });
